b61c780c6b7ae95c05824b7d23087fa006434c07
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-08 15:16:10 -0400

    Add restrictions logic for stock tracking

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
   # validations
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+
+  def can_track_stock?(ticker_symbol)
+    within_stock_limit? && !alread_tracking_stock?(ticker_symbol)
+  end
+
+  def alread_tracking_stock?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+
+    stocks.where(id: stock.id).exists?
+  end
+  def within_stock_limit?
+    stocks.count < 10
+  end
 end
