4e3144c0b9beddc166bfd1faa0b51b8d35356418
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-08 19:49:10 -0400

    Add friend search result display partial and implement search functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,30 @@
 class UsersController < ApplicationController
+  def my_friends
+    @friends = current_user.friends
+  end
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
 
-  def my_friends
-    @friends = current_user.friends
+  def search
+    if params[:friend]. present?
+      @friends = User.search(params[:friend])
+      if @friends.any?
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "No results found. Invite them to sign up!"
+          format.js { render partial: 'users/friend_result' }
+        end        
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Unless you like hitting buttons, please enter a value to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,11 +21,35 @@ class User < ApplicationRecord
     stocks.where(id: stock.id).exists?
   end
 
+  def already_following?(username)
+    return true
+  end
+
   def can_track_stock?(ticker_symbol)
     within_stock_tracking_limit? && !alread_tracking_stock?(ticker_symbol)
   end
 
   def within_stock_tracking_limit?
     stocks.count < 10
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.name_matches(param)
+    (matches('first_name ', param) + matches('last_name',param) + 
+      matches('username', param)).uniq
+  end
+
+  def self.search(param)
+    param.strip!
+    query_result = (email_matches(param) + name_matches(param)).uniq
+    return nil unless query_result
+    query_result
   end
 end
