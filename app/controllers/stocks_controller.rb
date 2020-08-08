035f1b2adfd401d64c36a38b0771d6902a9df793
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-08 05:31:02 -0400

    Validate search queries and render Ajax response

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,22 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock]. present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/stock_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "No results found"
+          format.js { render partial: 'users/stock_result' }
+        end        
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Unless you like hitting buttons, please enter a value to search"
+        format.js { render partial: 'users/stock_result' }
+      end
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,10 +8,14 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
    
-    new(
-      ticker: ticker_symbol,
-      name: client.company(ticker_symbol).company_name,
-      last_price: client.price(ticker_symbol)
-    )
+    begin
+      new(
+        ticker: ticker_symbol,
+        name: client.company(ticker_symbol).company_name,
+        last_price: client.price(ticker_symbol)
+      )
+    rescue => exception
+      return nil
+    end
   end
 end
