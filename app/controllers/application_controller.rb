a242b6b825306e598e76e5301d1a58de447bc62a
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-08 22:13:37 -0400

    Update registration workflow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :username, :remember_me])
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
       user_params.permit(:username, :email, :remember_me)
     end
