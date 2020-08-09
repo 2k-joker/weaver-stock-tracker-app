d8acef4ac98d4f45cb41065d8c0d1d4b52ce01c0
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-08 21:56:22 -0400

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
   resources :users, only: [:show]
-  devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_profile', to: 'users#my_profile'
