510f44991233819174042f5ae00fbce477530714
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-16 22:57:39 -0400

    Clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tracked_stocks = @user.stocks
+    @tracked_stocks = @user.stocks.order('id')
   end
 end
